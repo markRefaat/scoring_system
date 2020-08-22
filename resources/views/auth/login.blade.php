341e401db6d8725b0ef1aef3add411e654dd760e
fadybassel1 <56454311+fadybassel1@users.noreply.github.com>
2020-08-22 23:15:30 +0200

    update login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input style="text-transform:uppercase;" on keyup="javascript:this.value=this.value.toUpperCase();" id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
