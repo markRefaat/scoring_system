14b265e8b3b86409246cafddad480b9f00166516
Mark <mark.refaat.ramzy@gmail.com>
2020-08-22 19:21:28 +0200

    db changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             // $table->string('username')->unique();
             $table->integer('score');
+            $table->integer('staticScore');
             $table->string('email')->nullable();
             // $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
