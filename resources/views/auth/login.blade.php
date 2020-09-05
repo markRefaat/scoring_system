73e35382f3622f1d18f0960194d30333a06702a3
fadybassel1 <56454311+fadybassel1@users.noreply.github.com>
2020-09-05 23:34:52 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,12 +13,12 @@
                         <b>سوف يتم فتح تسجيل الهدايا يوم السبت بعد انتهاء المؤتمر</b>
                     </div> 
 
-                   {{--  <div class="alert alert-info text-center"><b>تم انتهاء موعد حجز الهدايا</b></div>
+                     <div class="alert alert-info text-center"><b>تم انتهاء موعد حجز الهدايا</b></div>
                     <div class="alert alert-warning text-center" role="alert">
-                        أستلام الهدايا من الكنيسة يوم الاحد 30/8 من الساعة 5 الى 8
+                         أستلام الهدايا من الكنيسة امام باب الحضانة يوم الاحد 6/9 من الساعة 5 الى 9
                         <br>
                         لمن لا يناسبه الموعد برجاء مراسلة فيلوباتير على الواتساب على الرقم 01203566808
-                    </div> --}}
+                    </div> 
 
 
                     {{-- <form method="POST" action="{{ route('login') }}">
